64c6db5898e84a51a5483eb62033a57b1a01c9ae
Lim Jitt Hing <jhlim.2023@smu.edu.sg>
2025-01-31 18:06:41 +0800

    update db + fetch countries with coordinates

--- backend/router/appRouter.js
+++ backend/router/appRouter.js
@@ -1,14 +1,19 @@
 const express = require("express");
 
-const {getRelationships} = require("../controllers/relationshipController");
 const { getGraphRelationships } = require("../controllers/neo4jController");
 
+const {
+  getRelationships,
+  updateCountries,
+  getCountriesWithCoor,
+} = require("../controllers/relationshipController");
 
 router = express.Router();
 
 // Relationship routes
 router.get("/relationships", getRelationships);
 router.get("/graph/relationships", getGraphRelationships);
-
+router.get("/update-coor", updateCountries);
+router.get("/get-country-coord", getCountriesWithCoor);
 
 module.exports = router;
